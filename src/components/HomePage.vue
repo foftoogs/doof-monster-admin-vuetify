f0bf7f76521c824482faa702e57b68d0698850d6
foftoogs <fof@brainstorm.net.au>
2023-05-28 20:48:07 +1000

    - simplified components a bit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+      <v-container>
+        <v-row>
+        <v-col cols="12">
+          <v-text-field
+            id="test"
+            v-model="testVal"
+            ></v-text-field>
+        </v-col>
+        </v-row>
+      </v-container>
+  </div>
+
+</template>
+
+<script>
+export default {
+  setup() {
+    // const userStore = useUserStore();
+    // return { userStore };
+  },
+  data() {
+    return {
+      testVal: 0
+    }
+  },
+  methods: {
+
+    test(){
+      console.log('test');
+    }
+    
+  },
+  mounted(){
+
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,13 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/components/HomePage.vue'
 
 const routes = [
   {
     path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
-    children: [
-      {
-        path: '',
-        name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
-      },
-    ],
-  },
+    name: 'Home',
+    component: Home
+  }
 ]
 
 const router = createRouter({
